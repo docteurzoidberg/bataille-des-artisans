2c0c933c21279bd848c1e5f580295cd6b4733489
TCM <tcm@alpMAC.tcm.lyon>
2020-01-13 13:52:13 +0100

    fix typos

--- www-bataille/pages/unboxing.vue
+++ www-bataille/pages/unboxing.vue
@@ -10,7 +10,7 @@
         <v-card-text class="text-left">
           <p>Salut les bagarreurs!</p>
 
-          <p>Vous avez recu votre colis? Vous vous demandez ce que c'est ? Si ca va exploser ? Nous tenterons ici de repondre a (presque) toutes vos questions!</p>
+          <p>Vous avez recu votre colis ? Vous vous demandez ce que c'est ? Si ca va exploser ? Nous tenterons ici de repondre a (presque) toutes vos questions!</p>
 
           <h2 id="contenuducolis">
             Contenu du colis
@@ -27,12 +27,12 @@
           <p>(image)</p>
 
           <h2 id="lezillominidevkitcestquoi">
-            Le zillo-mini-devkit c'est quoi???
+            Le zillo-mini-devkit c'est quoi ???
           </h2>
 
-          <p>C'est une plateforme d'immagination et de developpement pour la Bataille-des-artisans</p>
+          <p>C'est une plateforme d'imagination et de développement pour la Bataille-des-artisans</p>
 
-          <p>Son hardware est le suivant:</p>
+          <p>Son hardware est le suivant :</p>
 
           <h3 id="esp32wroom">
             ESP32-WROOM
@@ -53,14 +53,14 @@
           </p>
           <p>
             L'ESP32 est un module 'tout en un' comprenant de nombreuses fonctionnalités.
-            C'est un microcontroleur 32bit multi-coeur cadence a xx mhz.
-            Il dispose nativement de fonctionnalités WIFI, de ports série, d'un systeme de fichiers minimaliste et d'une memoire flash de 4mo, permettant de stocker programme et assets.
+            C'est un microcontroleur 32bit multi-coeur cadencé a xx MHz.
+            Il dispose nativement de fonctionnalités WIFI, de ports série, d'un systême de fichiers minimaliste et d'une mémoire flash de 4 Mo, permettant de stocker programme et assets.
             (liste de fonctionnalités non exhaustive)
           </p>
 
           <p>Il est programmable via son port USB avec un framework Arduino.</p>
 
-          <p>Plus d'explications l'ESP32. avec des docs, a venir !</p>
+          <p>Plus d'explications sur l'ESP32 avec des documents, à venir !</p>
 
           <h3 id="matricedeledsws2812b">
             Matrice de leds WS2812b
@@ -81,11 +81,11 @@
             </v-card>
           </p>
 
-          <p>C'est une matrice de leds RGB adressables (c'est a dire qu'elle sont controlables individuellement), qui utilise un bus de communication entre les leds pour passer le signal de led en led.</p>
+          <p>C'est une matrice de leds RGB adressables (c'est a dire qu'elles sont controlables individuellement), qui utilise un bus de communication entre les leds pour passer le signal de led en led.</p>
 
-          <p>Des librairies permettront le pilotage de ces leds sans se preocupper de leur protocole de communication.</p>
+          <p>Des librairies permettront le pilotage de ces leds sans se préocupper de leur protocole de communication.</p>
 
-          <p>Plus d'infos sur les leds WS2812b communement appeles "NEOPIXELS", et leur matrices  sur le internet par ici:</p>
+          <p>Plus d'infos sur les leds WS2812b communément appelées "NEOPIXELS", et leur matrices  sur internet par ici :</p>
 
           <p>(liens vers la doc adafruit a venir)</p>
 
@@ -109,15 +109,15 @@
           </p>
 
           <p>
-            Le module MPU6050 est une puce tout-en-un qui contient 3 accelerometres et 3 gyroscopes.
-            Il permet ainsi d'obtenir des informations concernant l'acceleration et l'orientation de l'objet.
+            Le module MPU6050 est une puce tout-en-un qui contient 3 accéléromètres et 3 gyroscopes.
+            Il permet ainsi d'obtenir des informations concernant l'accélération et l'orientation de l'objet.
           </p>
 
           <p>La communication entre le programme et le MPU6050 s'effectue via le protocole I2C.</p>
 
           <p>Des librairies permettront l'interrogation facile de toutes ces valeurs.</p>
 
-          <p>(lien vers la doc a venir)</p>
+          <p>(lien vers la doc à venir)</p>
 
           <h3 id="micromax4466">
             Micro+MAX4466
@@ -138,13 +138,13 @@
             </v-card>
           </p>
 
-          <p>Le micro est relie a une entree analogique de l'ESP32 et permet par exemple de detecter le niveau de bruit ambiant.</p>
+          <p>Le micro est relié à une entrée analogique de l'ESP32 et permet par exemple de détecter le niveau de bruit ambiant.</p>
 
-          <p>Il ne sera malheureusement pas assez precis pour des usages plus aboutis, de maniere simple, dans notre projet.</p>
+          <p>Il ne sera malheureusement pas assez précis pour des usages plus aboutis, de manière simple, dans notre projet.</p>
 
-          <p>Cependant Fourrier, et ses transformations, est peut-etre notre ami! (ou pas)</p>
+          <p>Cependant Fourrier, et ses transformations, est peut-être notre ami! (ou pas)</p>
 
-          <p>(lien vers la doc a venir)</p>
+          <p>(lien vers la doc à venir)</p>
 
           <h3 id="buzzerdisque">
             Buzzer disque
@@ -165,9 +165,9 @@
             </v-card>
           </p>
 
-          <p>Le buzzer est relié directement a une sortie PWM de l'ESP32. Le PWM permet de moduler la frequence d'un signal, ce qui a pour effet de produire des tonalités ou melodies lorsque connecté a un haut parleur basique.</p>
+          <p>Le buzzer est relié directement à une sortie PWM de l'ESP32. Le PWM permet de moduler la fréquence d'un signal, ce qui a pour effet de produire des tonalités ou mélodies lorsque connecté à un haut parleur basique.</p>
 
-          <p>Dans notre projet il n'y a malheureusement pas de circuit d'amplification du signal, ni de haut parleur assez puissant permettant de faire un son "fort"</p>
+          <p>Dans notre projet il n'y a malheureusement pas de circuit d'amplification du signal, ni de haut parleur assez puissant permettant de faire un son "fort".</p>
 
           <h3 id="uncircuitimprimpcbpermettantdeconnecterletout">
             Un circuit imprimé (PCB) permettant de connecter le tout

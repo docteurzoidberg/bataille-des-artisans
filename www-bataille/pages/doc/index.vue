fac48ddb3fe11221ca2a3a5f9891e3369294f821
DrZoid <docteurzoidberg@users.noreply.github.com>
2020-01-19 20:03:49 +0100

    page setup environnement de dev

--- www-bataille/pages/doc/index.vue
+++ www-bataille/pages/doc/index.vue
@@ -4,13 +4,13 @@
       <nuxt-link to="/doc/setup">Setup son environnement de developpement</nuxt-link>
     </h2>
     <h3>
-      Reference Arduino: <a href="https://www.arduino.cc/reference/en/"> https://www.arduino.cc/reference/en/</a>
+      Reference Arduino: <a href="https://www.arduino.cc/reference/en/" target="_blank"> https://www.arduino.cc/reference/en/</a>
     </h3>
     <h3>
-      Reference lib Adafruit_GFX: <a href="https://cdn-learn.adafruit.com/downloads/pdf/adafruit-gfx.pdf"> https://cdn-learn.adafruit.com/downloads/pdf/adafruit-gfx.pdf</a>
+      Reference lib Adafruit_GFX: <a href="https://cdn-learn.adafruit.com/downloads/pdf/adafruit-gfx.pdf" target="_blank"> https://cdn-learn.adafruit.com/downloads/pdf/adafruit-gfx.pdf</a>
     </h3>
     <h3>
-      Documentation lib Fastled: <a href="https://github.com/FastLED/FastLED/wiki/Overview"> https://github.com/FastLED/FastLED/wiki/Overview</a>
+      Documentation lib Fastled: <a href="https://github.com/FastLED/FastLED/wiki/Overview" target="_blank"> https://github.com/FastLED/FastLED/wiki/Overview</a>
     </h3>
   </Page>
 </template>
--- www-bataille/pages/doc/setup.vue
+++ www-bataille/pages/doc/setup.vue
@@ -1,6 +1,42 @@
 <template>
   <Page title="Setup son environnement de developpement" icon="mdi-tools">
-    <h2>A venir</h2>
+
+    <h2>Pre-requis</h2>
+
+    <h3>Logiciels a avoir</h3>
+
+    <h4>
+      Visual Studio Code
+      <a href="https://code.visualstudio.com/download" target="_blank">(lien)</a>
+    </h4>
+    <p>On ne vous le présente plus !</p>
+
+    <h4>
+      PIO (extension vscode)
+      <a href="https://platformio.org/install/ide?install=vscode" target="_blank">(lien)</a>
+    </h4>
+    <p>
+      Extension permettant l'integration de multiples frameworks IOT au sein de vscode.<br/>
+      PIO gerera pour vous l'installation du framework arduino, des librairies, de compiler le code, de flasher l'esp32 (etc), d'ouvrir le moniteur serie (entre autres)
+    </p>
+
+    <h4>
+      OpenScad
+      <a href="https://www.openscad.org/downloads.html" target="_blank">(lien)</a>
+    </h4>
+    <p>
+      Logiciel de 3d parametrique.<br/>
+      Vous permetra d'editer les sources du boitier et du diffuseur.
+    </p>
+
+    <h4>
+      KiCad (facultatif)
+      <a href=" https://kicad-pcb.org/download/" target="_blank">(lien)</a>
+    </h4>
+    <p>
+      Outil "EDA" de conception de circuits electroniques.<br/>
+      Vous permetra de consulter les sources du schéma electronique et du circuit imprimé.
+    </p>
   </Page>
 </template>
 

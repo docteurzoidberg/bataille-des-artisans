05121e4554131e484988a333531751d04aeea1b8
DrZoid <docteurzoidberg@users.noreply.github.com>
2020-01-19 19:23:32 +0100

    ajouts liens doc

--- www-bataille/pages/doc/index.vue
+++ www-bataille/pages/doc/index.vue
@@ -1,8 +1,17 @@
 <template>
   <Page title="Documentation" icon="mdi-book">
     <h2>
-      <nuxt-link to="/doc/setup">Setup</nuxt-link>
+      <nuxt-link to="/doc/setup">Setup son environnement de developpement</nuxt-link>
     </h2>
+    <h3>
+      Reference Arduino: <a href="https://www.arduino.cc/reference/en/"> https://www.arduino.cc/reference/en/</a>
+    </h3>
+    <h3>
+      Reference lib Adafruit_GFX: <a href="https://cdn-learn.adafruit.com/downloads/pdf/adafruit-gfx.pdf"> https://cdn-learn.adafruit.com/downloads/pdf/adafruit-gfx.pdf</a>
+    </h3>
+    <h3>
+      Documentation lib Fastled: <a href="https://github.com/FastLED/FastLED/wiki/Overview"> https://github.com/FastLED/FastLED/wiki/Overview</a>
+    </h3>
   </Page>
 </template>
 
